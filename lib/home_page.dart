e10ca5d9dd1473a1de040887529335d11b6e0b60
Shashidhar Devulapally <devulapallysoujanya@gmail.com>
2022-11-29 10:43:50 +0530

    firstcommit

---
+++ lib/home_page.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    
+    return Scaffold(
+      appBar: AppBar(
+        // ignore: prefer_const_constructors
+        title: Text("Shopping App"),
+      ),
+      // ignore: prefer_const_constructors
+      body: Text('Welcome'),
+      // ignore: prefer_const_constructors
+      drawer: Drawer(),
+    );
+  }
+}
